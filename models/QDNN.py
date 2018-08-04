0a86e08c124bddd7dbc304422dee238ab7c5311c
waby <wabyking@163.com>
2018-08-04 20:23:16 +0200

    ~

--- models/QDNN.py
+++ models/QDNN.py
@@ -31,9 +31,10 @@ class QDNN(BasicModel):
         self.phase_embedding=phase_embedding_layer(self.opt.max_sequence_length, self.opt.lookup_table.shape[0], self.opt.lookup_table.shape[1], trainable = self.opt.embedding_trainable)
         self.amplitude_embedding = amplitude_embedding_layer(np.transpose(self.opt.lookup_table), self.opt.max_sequence_length, trainable = self.opt.embedding_trainable, random_init = self.opt.random_init)
         self.weight_embedding = Embedding(self.opt.lookup_table.shape[0], 1, trainable = True)
-        self.dense = Dense(self.opt.nb_classes, activation="sigmoid", kernel_regularizer= regularizers.l2(0.0001))        
+        self.dense = Dense(self.opt.nb_classes, activation="sigmoid", kernel_regularizer= regularizers.l2(0.0001))  # activation="sigmoid",       
         self.dropout = Dropout(self.opt.dropout_rate)
-        
+        self.dense2 = Dense(10, activation="sigmoid", kernel_regularizer= regularizers.l2(0.0001))
+        self.dense3 = Dense(self.opt.nb_classes, activation="sigmoid", kernel_regularizer= regularizers.l2(0.0001))
     def __init__(self,opt):
         super(QDNN, self).__init__(opt) 
         
@@ -61,7 +62,7 @@ class QDNN(BasicModel):
     
         probs = ComplexMeasurement(units = 10)([sentence_embedding_real, sentence_embedding_imag])
 #        probs = self.dropout(probs)
-        output =  self.dense(probs)
+        output =  self.dense3(self.dense2(probs))
     
         model = Model(self.doc, output)
     
