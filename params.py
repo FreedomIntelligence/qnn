6f01ce6a2ff439ba9e0d8e823aa333bdccd18443
waby <wabyking@163.com>
2018-08-31 22:26:39 +0200

    add para in params.py

--- params.py
+++ params.py
@@ -47,9 +47,10 @@ class Params(object):
         for k, v in self.__dict__.items():
             if k in ['dataset_name','batch_size','epochs','network_type',
                      'dropout_rate_embedding','dropout_rate_probs','measurement_size',
-                     'lr','ngram_value','clean','remove_punctuation',
+                     'lr','ngram_value','clean',
                      'match_type','margin','pooling_type','steps_per_epoch',
-                     'distance_type','embedding_size']:
+                     'distance_type','embedding_size',"max_len",
+                     'remove_punctuation',"remove_stowords","clean_sentence",  "train_verbose","stem"]:
                 info.append("%s -> %s"%(k,str(v)))
         return info
     def to_string(self):
@@ -59,4 +60,17 @@ class Params(object):
             f.write("\n".join(self.get_parameter_list()))
         
 
-
+clean_sentence = 1
+random_init = 0 ; attention
+match_type = pointwise
+margin = 0.1
+pooling_type = max
+steps_per_epoch = 100
+distance_type = 6
+onehot = 1
+max_len = 50
+output_file = output_point_wiki.txt
+train_verbose = 1
+remove_punctuation= 0
+stem = 0
+remove_stowords = 0
