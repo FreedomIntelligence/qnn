6d26d1d4a4e6daa25256c826fe5329827415eb2e
waby <wabyking@163.com>
2018-09-01 12:22:40 +0200

    fix some minor bugs

--- testMatch.py
+++ testMatch.py
@@ -104,26 +104,26 @@ if __name__ == '__main__':
         "measurement_size" :[100],#,50100],
 #        "ngram_value":["1,2,3","2,3,4","1,3,4"],
 #        "margin":[0.1,0.2],
-        "lr" : [0.1,0.01],#,1,0.01
+        "lr" : [0.1],#,1,0.01
 #        "dropout_rate_embedding" : [0.9],#0.5,0.75,0.8,0.9,1],
 #        "dropout_rate_probs" : [0.8,0.9]#,0.5,0.75,0.8,1]   
 #            "ngram_value" : [3]
         "max_len":[100],
-        "one_hot": [0,1],
+        "one_hot": [0],
         "distance_type":[6],
-        "train_verbose":[0],
+#        "train_verbose":[0],
         "remove_punctuation": [0],
         "stem" : [0],
         "remove_stowords" : [0]
     }
     import argparse
     import itertools
-
+    
     params = Params()
     parser = argparse.ArgumentParser(description='running the complex embedding network')
     parser.add_argument('-gpu_num', action = 'store', dest = 'gpu_num', help = 'please enter the gpu num.',default=1)
     parser.add_argument('-gpu', action = 'store', dest = 'gpu', help = 'please enter the gpu num.',default=0)
-    parser.add_argument('-config', action = 'store', dest = 'config', help = 'please enter the config path.',default='config/qalocal_point_wiki.ini')
+    parser.add_argument('-config', action = 'store', dest = 'config', help = 'please enter the config path.',default='config/msra.ini')
     args = parser.parse_args()
     parameters= [arg for index,arg in enumerate(itertools.product(*grid_parameters.values())) if index%args.gpu_num==args.gpu]
     
@@ -161,7 +161,10 @@ if __name__ == '__main__':
                     optimizer = units.getOptimizer(name=params.optimizer,lr=params.lr),
                     metrics=[metric_type])
             for i in range(params.epochs):
-                model.fit_generator(reader.getPointWiseSamples4Keras(onehot = params.onehot),epochs = 1,steps_per_epoch=int(len(reader.datas["train"])/reader.batch_size),verbose = True)        
+                if "unbalance" in  params.__dict__ and params.unbalance:
+                    model.fit_generator(reader.getPointWiseSamples4Keras(onehot = params.onehot,unbalance=params.unbalance),epochs = 1,steps_per_epoch=int(len(reader.datas["train"])/reader.batch_size),verbose = True)        
+                else:
+                    model.fit_generator(reader.getPointWiseSamples4Keras(onehot = params.onehot),epochs = 1,steps_per_epoch=int(len(reader.datas["train"])/reader.batch_size),verbose = True)        
                 y_pred = model.predict(x = test_data) 
                 score =batch_softmax_with_first_item(y_pred)[:,1]  if params.onehot else y_pred
                 
