807bb7f2c43044659b270ed68b25b4bff22f5f8a
Qiuchi Li <qiuchi.li@studenti.unipd.it>
2018-11-17 11:46:41 +0100

    ~

--- models/embedding/ComplexWordEmbedding.py
+++ models/embedding/ComplexWordEmbedding.py
@@ -8,14 +8,10 @@ from layers.keras.complexnn import *
 
 from keras.initializers import Constant
 import numpy as np
+
 class ComplexWordEmbedding(BasicModel):
     
     def initialize(self):
-       
-#        self.phase_embedding=phase_embedding_layer(self.opt.max_sequence_length, self.opt.lookup_table.shape[0], self.opt.lookup_table.shape[1], trainable = self.opt.embedding_trainable)
-#                
-#        self.amplitude_embedding = amplitude_embedding_layer(np.transpose(self.opt.lookup_table), self.opt.max_sequence_length, trainable = self.opt.embedding_trainable, random_init = self.opt.random_init)
-#
 #        
         self.amplitude_embedding = amplitude_embedding_layer(np.transpose(self.opt.lookup_table), None, trainable = self.opt.embedding_trainable, random_init = self.opt.random_init,l2_reg=self.opt.amplitude_l2)
         self.phase_embedding= phase_embedding_layer(None, self.opt.lookup_table.shape[0], self.opt.lookup_table.shape[1], trainable = self.opt.embedding_trainable,l2_reg=self.opt.phase_l2)
