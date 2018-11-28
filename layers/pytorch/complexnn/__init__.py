b71565ab6aab4ec59034f3bbc84af1d088171336
WhereIsMyHead <gene_zhangchen@163.com>
2018-11-28 15:44:01 +0800

    Fix bugs in layers.pytorch.measurement and make test_torch_qdnn runable

--- layers/pytorch/complexnn/__init__.py
+++ layers/pytorch/complexnn/__init__.py
@@ -10,12 +10,11 @@ from layers.pytorch.complexnn.measurement import ComplexMeasurement
 from layers.pytorch.complexnn.concatenation import Concatenation
 from layers.pytorch.complexnn.index import Index
 from layers.pytorch.complexnn.ngram import NGram
-from layers.pytorch.complexnn.utils import GetReal
-from layers.pytorch.complexnn.projection import Complex1DProjection
+#from layers.pytorch.complexnn.projection import Complex1DProjection
 from layers.pytorch.complexnn.l2_norm import L2Norm
 from layers.pytorch.complexnn.l2_normalization import L2Normalization
-from layers.pytorch.complexnn.utils import *
-from layers.pytorch.complexnn.reshape import reshape
+#from layers.pytorch.complexnn.utils import *
+#from layers.pytorch.complexnn.reshape import reshape
 from layers.pytorch.complexnn.lambda_functions import *
-from layers.pytorch.complexnn.cosine import Cosinse
-from layers.pytorch.complexnn.marginLoss import MarginLoss
+from layers.pytorch.complexnn.cosine import Cosine
+from layers.pytorch.complexnn.margin_loss import MarginLoss
--- layers/pytorch/complexnn/measurement.py
+++ layers/pytorch/complexnn/measurement.py
@@ -3,18 +3,14 @@
 import torch
 import torch.nn.functional as F
 import torch.nn
-from torch.autograd import Variable
-from layers.keras.complexnn.measurement import ComplexMeasurement as ComplexMeasurement2
-import numpy as np
-from keras.models import Model, Input
 
 class ComplexMeasurement(torch.nn.Module):
-
-    def __init__(self, units = 5, trainable = True):
+    def __init__(self, embed_dim, units=5):
         super(ComplexMeasurement, self).__init__()
-#        self.average_weights = average_weights
         self.units = units
-        
+        self.embed_dim = embed_dim
+        kernel = torch.nn.Parameter(torch.rand(self.units, embed_dim, 2))
+        self.kernel = F.normalize(kernel.view(self.units, -1), p=2, dim=1, eps=1e-10).view(self.units, embed_dim, 2)
 
     def forward(self, inputs):
 
@@ -27,84 +23,36 @@ class ComplexMeasurement(torch.nn.Module):
                             'on a list of 2 inputs.'
                             'Got ' + str(len(inputs)) + ' inputs.')
         
-       
-        embedding_dimension = inputs[0].shape[-1]
-        batch_size = inputs[0].shape[0]
+        batch_size = inputs[0].size(0)
         
-        
-        self.kernel = torch.nn.Parameter(torch.rand(self.units,embedding_dimension,2))
-        
-        kernel = F.normalize(self.kernel.view(self.units, embedding_dimension*2), p=2, dim=1, eps=1e-10).view(self.units,embedding_dimension,2)
-        #self.kernel.shape = (self.units, embedding_dimension,2)
-        
-        kernel_real = kernel[:,:,0]
-        kernel_imag = kernel[:,:,1]
-        #kernel_real.shape = (self.units, embedding_dimension)
-        #kernel_imag.shape = (self.units, embedding_dimension)
+        kernel_real = self.kernel[:,:,0].unsqueeze(-1)
+        kernel_imag = self.kernel[:,:,1].unsqueeze(-1)
         
         input_real = inputs[0]
-        input_imag = inputs[1] 
+        input_imag = inputs[1]
         
-        #input_real.shape = (batch_size,embedding_dimension,embedding_dimension)
-        #input_imag.shape = (batch_size,embedding_dimension,embedding_dimension)
-        
-        projector_real = torch.bmm(kernel_real.view(self.units, embedding_dimension,1), kernel_real.view(self.units, 1,embedding_dimension)) \
-            + torch.bmm(kernel_imag.view(self.units, embedding_dimension,1), kernel_imag.view(self.units, 1,embedding_dimension))
+        projector_real = torch.bmm(kernel_real, kernel_real.transpose(1, 2)) \
+            + torch.bmm(kernel_imag, kernel_imag.transpose(1, 2))
             
-        projector_imag = torch.bmm(kernel_imag.view(self.units, embedding_dimension,1), kernel_real.view(self.units, 1,embedding_dimension)) \
-            -torch.bmm(kernel_real.view(self.units, embedding_dimension,1), kernel_imag.view(self.units, 1,embedding_dimension))
+        projector_imag = torch.bmm(kernel_imag, kernel_real.transpose(1, 2)) \
+            - torch.bmm(kernel_real, kernel_imag.transpose(1, 2))
+
+        output_real = torch.mm(input_real.view(batch_size, self.embed_dim*self.embed_dim), projector_real.view(self.units,self.embed_dim*self.embed_dim).t())\
+        - torch.mm(input_imag.view( batch_size, self.embed_dim*self.embed_dim), projector_imag.view(self.units,self.embed_dim*self.embed_dim).t())
         
-        projector_real = projector_real
-        projector_imag = projector_imag
-        #projector_real.shape = (self.units,embedding_dimension,embedding_dimension)
-        #projector_imag.shape = (self.units,embedding_dimension,embedding_dimension)
-        output_real = torch.mm(input_real.view(batch_size, embedding_dimension*embedding_dimension), projector_real.view(self.units,embedding_dimension*embedding_dimension).permute(1,0))\
-        - torch.mm(input_imag.view( batch_size, embedding_dimension*embedding_dimension), projector_imag.view(self.units,embedding_dimension*embedding_dimension).permute(1,0))
+        output_imag = torch.mm(input_real.view(batch_size, self.embed_dim*self.embed_dim), projector_imag.view(self.units,self.embed_dim*self.embed_dim).t())\
+        + torch.mm(input_imag.view(batch_size, self.embed_dim*self.embed_dim), projector_real.view(self.units,self.embed_dim*self.embed_dim).t())
         
-        output_imag = torch.mm(input_real.view(batch_size, embedding_dimension*embedding_dimension), projector_imag.view(self.units,embedding_dimension*embedding_dimension).permute(1,0))\
-        + torch.mm(input_imag.view(batch_size, embedding_dimension*embedding_dimension), projector_real.view(self.units,embedding_dimension*embedding_dimension).permute(1,0))
-        
-       
-        output = output_real+output_imag
-        return output_real, output_imag
+    
+        return [output_real, output_imag]
     
 if __name__ == '__main__':
+    model = ComplexMeasurement(6, units=3)
+    a = torch.randn(5,6,6)
+    b = torch.randn(5,6,6)
 
-        
-    from qutip import rand_dm  
-    reals, images = [] ,[]
-    model = ComplexMeasurement(3)
-    for i in range(4):
-        m = rand_dm(5)
-        images.append( m.data.toarray().imag)
-        reals.append( m.data.toarray().real)
-    
-    x= torch.from_numpy(np.array(reals,dtype="float32"))
-    y = torch.from_numpy(np.array(images,dtype="float32"))
-
-    y_pred = model([x,y])
-#    input_2 = Input(shape=(4,5,5), dtype='float')
-#    output = ComplexMeasurement(3)([input_1,input_2])
-#
-#
-#    model = Model([input_1,input_2], output)
-#    model.compile(loss='binary_crossentropy',
-#              optimizer='sgd',
-#              metrics=['accuracy'])
-#    model.summary()
-#
-#    weights = model.get_weights()
-#    x_1 = np.random.random((5,4,5,5))
-#    x_2 = np.random.random((5,4,5,5))
-#    output = model.predict([x_1,x_2])
-#    for i in range(5):
-#        xy = x_1[i] + 1j * x_2[i]
-#        for j in range(3):
-#
-#            m= weights[0][j,:,0] + 1j *weights[0][j,:,1]
-##            print(np.matmul(xy[0] ,np.outer(m,m)))
-##            result = np.absolute(np.trace(np.matmul(xy ,np.outer(m,m))))
-#            print(np.trace(np.matmul(xy[0] ,np.outer(m,m))))    
+    y_pred = model([a,b])
+    print(y_pred[0])
     
     
     
