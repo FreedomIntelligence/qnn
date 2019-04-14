c65e285d99883152b5f019eaa38600a3c1e42fca
qiuchi li <qiuchi.li@studenti.unipd.it>
2019-04-14 20:35:46 +0200

    restructure run_cnm.py

--- run_cnm.py
+++ run_cnm.py
@@ -6,7 +6,7 @@ import keras.backend as K
 import pandas as pd
 from layers.loss import *
 from layers.loss.metrics import precision_batch
-from tools.units import to_array, getOptimizer, batch_softmax_with_first_item
+from tools.units import to_array, getOptimizer
 import argparse
 import itertools
 from numpy.random import seed
@@ -14,11 +14,66 @@ from tensorflow import set_random_seed
 import tensorflow as tf
 import os
 import random
+from models import match as models   
+from tools.evaluation import matching_score
 
+def run(params,reader):
+    test_data = reader.getTest(iterable = False, mode = 'test')
+    dev_data = reader.getTest(iterable = False, mode = 'dev')
+    qdnn = models.setup(params)
+    model = qdnn.getModel()
 
+    history=[]
+    data_generator = None
+    if 'onehot' not in params.__dict__:
+        params.onehot = 0
+    if params.match_type == 'pointwise':
+        test_data = [to_array(i,reader.max_sequence_length) for i in test_data]
+        dev_data = [to_array(i,reader.max_sequence_length) for i in dev_data]
+        if params.onehot:
+            loss_type,metric_type = ("categorical_hinge","acc") 
+        else:
+            loss_type,metric_type = ("mean_squared_error","mean_squared_error")
+            
+        model.compile(loss =loss_type, #""
+                optimizer = getOptimizer(name=params.optimizer,lr=params.lr),
+                metrics=[metric_type])
+        data_generator = reader.getPointWiseSamples4Keras(onehot = params.onehot)
+#            if "unbalance" in  params.__dict__ and params.unbalance:
+#                model.fit_generator(reader.getPointWiseSamples4Keras(onehot = params.onehot,unbalance=params.unbalance),epochs = 1,steps_per_epoch=int(len(reader.datas["train"])/reader.batch_size),verbose = True)        
+#            else:
+#                model.fit_generator(reader.getPointWiseSamples4Keras(onehot = params.onehot),epochs = 1,steps_per_epoch=len(reader.datas["train"]["question"].unique())/reader.batch_size,verbose = True)        
+    elif params.match_type == 'pairwise':
+        test_data.append(test_data[0])
+        test_data = [to_array(i,reader.max_sequence_length) for i in test_data]
+        dev_data.append(dev_data[0])
+        dev_data = [to_array(i,reader.max_sequence_length) for i in dev_data]
+        model.compile(loss = identity_loss,
+                optimizer = getOptimizer(name=params.optimizer,lr=params.lr),
+                metrics=[precision_batch],
+                loss_weights=[0.0, 1.0,0.0])
+        data_generator = reader.getPairWiseSamples4Keras()
+          
+    print('Training the network:')
+    for i in range(params.epochs):
+        model.fit_generator(data_generator,epochs = 1,steps_per_epoch=int(len(reader.datas["train"]["question"].unique())/reader.batch_size),verbose = True)          
+        print('Validation Performance:')
+        y_pred = model.predict(x = dev_data) 
+        score = matching_score(y_pred, params.onehot, params.match_type)
+        metric = reader.evaluate(score, mode = "dev")
+        history.append(metric)
+        print(metric)
+    print('Done.')
+    print('Test Performance:')
+    y_pred = model.predict(x = test_data) 
+    score = matching_score(y_pred, params.onehot, params.match_type)    
+    metric = reader.evaluate(score, mode = "test")
+    print(metric)
+              
+    return history, metric
+            
 
-
-
+    
 if __name__ == '__main__':
 #def test_match():
     
@@ -54,7 +109,7 @@ if __name__ == '__main__':
 
 
     params = Params()
-    parser = argparse.ArgumentParser(description='Running the Complex-valued Network for Matching')
+    parser = argparse.ArgumentParser(description='Running the Complex-valued Network for Matching.')
     parser.add_argument('-gpu_num', action = 'store', dest = 'gpu_num', help = 'please enter the gpu num.',default=1)
     parser.add_argument('-gpu', action = 'store', dest = 'gpu', help = 'please enter the gpu num.',default=0)
     parser.add_argument('-config', action = 'store', dest = 'config', help = 'please enter the config path.',default='config/qalocal_pair_trec.ini')
@@ -80,75 +135,16 @@ if __name__ == '__main__':
 #            print("switch %s to %s"%(old_dataset,params.dataset_name))
 #            reader=dataset.setup(params)
 #            params.reader = reader
-        from models import match as models      
+   
         reader = qa.setup(params)
-        test_data = reader.getTest(iterable = False)
-        dev_data = reader.getTest(iterable = False, mode = 'dev')
-        qdnn = models.setup(params)
-        model = qdnn.getModel()
-    
-        evaluations=[]
-        if params.match_type == 'pointwise':
-            if params.onehot:
-                params.lr = 10 *params.lr
-            test_data = [to_array(i,reader.max_sequence_length) for i in test_data]
-            loss_type,metric_type = ("categorical_hinge","acc") if params.onehot else ("mean_squared_error","mean_squared_error")
-            model.compile(loss =loss_type, #""
-                    optimizer = getOptimizer(name=params.optimizer,lr=params.lr),
-                    metrics=[metric_type])
-            
-            for i in range(params.epochs):
-                if "unbalance" in  params.__dict__ and params.unbalance:
-                    model.fit_generator(reader.getPointWiseSamples4Keras(onehot = params.onehot,unbalance=params.unbalance),epochs = 1,steps_per_epoch=int(len(reader.datas["train"])/reader.batch_size),verbose = True)        
-                else:
-                    model.fit_generator(reader.getPointWiseSamples4Keras(onehot = params.onehot),epochs = 1,steps_per_epoch=len(reader.datas["train"]["question"].unique())/reader.batch_size,verbose = True)        
-                
-                print('Validation Performance:')
-                y_pred = model.predict(x = dev_data) 
-                score =batch_softmax_with_first_item(y_pred)[:,1]  if params.onehot else y_pred
-                
-                metric = reader.evaluate(score, mode = "dev")
-                evaluations.append(metric)
-                print(metric)
-                
-            print('Test Performance:')
-            y_pred = model.predict(x = test_data) 
-            score =batch_softmax_with_first_item(y_pred)[:,1]  if params.onehot else y_pred
-                
-            metric = reader.evaluate(score, mode = "test")
-            print(metric)
-            
-            df=pd.DataFrame(evaluations,columns=["map","mrr","p1"])
-            file_writer.write(params.to_string()+'\n')
-            file_writer.write(str(df.max())+'\n')
-            file_writer.write('_________________________\n\n\n')
-            file_writer.flush()
-            K.clear_session()
+        history, performance = run(params, reader)
+        df=pd.DataFrame([list(performance)],columns=["map","mrr","p1"])
+        file_writer.write(params.to_string()+'\n')
+        file_writer.write(df+'\n')
+        file_writer.write('_________________________\n\n\n')
+        file_writer.flush()
+        K.clear_session()
         
-              
-        elif params.match_type == 'pairwise':
-            test_data.append(test_data[0])
-            test_data = [to_array(i,reader.max_sequence_length) for i in test_data]
-            model.compile(loss = identity_loss,
-                    optimizer = getOptimizer(name=params.optimizer,lr=params.lr),
-                    metrics=[precision_batch],
-                    loss_weights=[0.0, 1.0,0.0])
-            
-            for i in range(params.epochs):
-                model.fit_generator(reader.getPairWiseSamples4Keras(),epochs = 1,steps_per_epoch=int(len(reader.datas["train"]["question"].unique())/reader.batch_size),verbose = True)
-
-                y_pred = model.predict(x = test_data)
-                score = y_pred[0]
-#                score = batch_softmax_with_first_item(y_pred[0])[:,1]  if params.onehot else y_pred[0][:,1]
-                metric = reader.evaluate(score, mode = "test")
-                evaluations.append(metric)
-                print(metric)
-            df=pd.DataFrame(evaluations,columns=["map","mrr","p1"])
-            file_writer.write(params.to_string()+'\n')
-            file_writer.write(str(df.max())+'\n')
-            file_writer.write('_________________________\n\n\n')
-            file_writer.flush()
-            K.clear_session()
             
                 
     
